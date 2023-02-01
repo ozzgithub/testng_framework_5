a7cc60e6c3afe72c42cd949e37952db41d74800f
OZGE CAGLAYAN <oezgecaglayan@gmail.com>
2023-01-31 21:52:58 -0500

    added changes

--- src/test/java/pages/TechGlobalActionsPage.java
+++ src/test/java/pages/TechGlobalActionsPage.java
@@ -9,31 +9,34 @@ public class TechGlobalActionsPage extends TechGlobalBasePage{
         super();
     }
 
-    @FindBy(id = "")
+    @FindBy(id = "click")
     public WebElement clickBox;
 
-    @FindBy(id = "")
+    @FindBy(id = "click_result")
     public WebElement clickResult;
 
-    @FindBy(id = "")
+    @FindBy(id = "right-click")
     public WebElement rightClickBox;
 
-    @FindBy(id = "")
+    @FindBy(id = "right_click_result")
     public WebElement rightClickResult;
 
-    @FindBy(id = "")
+    @FindBy(id = "double-click")
     public WebElement doubleClickBox;
 
-    @FindBy(id = "")
+    @FindBy(id = "double_click_result")
     public WebElement doubleClickResult;
 
-    @FindBy(id = "")
+    @FindBy(id = "drag_element")
     public WebElement DragMeBox;
 
-    @FindBy(id = "")
+    @FindBy(id = "drag_and_drop_result")
+    public WebElement dragAndDropResult;
+
+    @FindBy(id = "drop_element")
     public WebElement dropHereBox;
 
-    @FindBy(id = "")
+    @FindBy(id = "input_box")
     public WebElement inputBox;
 
 
--- src/test/java/scripts/TechGlobalActionsTest.java
+++ src/test/java/scripts/TechGlobalActionsTest.java
@@ -1,12 +1,16 @@
 package scripts;
 
 import org.junit.Before;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.TechGlobalActionsPage;
 import pages.TechGlobalFrontendTestingHomePage;
+import utilities.Waiter;
+
+import java.time.Duration;
 
 public class TechGlobalActionsTest extends TechGlobalBase {
 
@@ -45,15 +49,104 @@ public class TechGlobalActionsTest extends TechGlobalBase {
         Assert.assertEquals(techGlobalActionsPage.clickResult.getText(), "You clicked on a button!");
 
         actions.moveToElement(techGlobalActionsPage.rightClickBox).contextClick().perform();
-        Assert.assertEquals(techGlobalActionsPage.rightClickResult.getText() , "Right-Click on me");
+        Assert.assertEquals(techGlobalActionsPage.rightClickResult.getText() , "You right-clicked on a button!");
 
         actions.moveToElement(techGlobalActionsPage.doubleClickBox).doubleClick().perform();
         Assert.assertEquals(techGlobalActionsPage.doubleClickResult.getText(), "You double-clicked on a button!");
+
+    }
+
+    /**
+     * Go to https://techglobal-training.netlify.app/
+     * Click on the "Practices" dropdown in the header
+     * Select the "Frontend Testing" option from the dropdown menu
+     * Click on the "Actions" card
+     * Verify that the last two web elements are present and labeled as "Drag Me", and "Drop Here",
+     * Perform a Drag and Drop action on the "Drag Me" web element, and drop it to "Drop Here"
+     * Verify that the first web element "Drag me" is successfully dropped into the second web element "Drop Here"
+     * Verify that a message appears next to the  web element stating "An element dropped here!"
+     */
+
+    @Test(priority = 2, description = "Validate last two web elements")
+    public void clickActions2() {
+
+//
+//        actions.moveToElement(techGlobalActionsPage.DragMeBox).
+//               clickAndHold().moveToElement(techGlobalActionsPage.dropHereBox).release().perform();
+
+        actions.dragAndDrop(techGlobalActionsPage.DragMeBox, techGlobalActionsPage.dropHereBox).perform();
+
+        Waiter.waitForVisibilityOfElement(techGlobalActionsPage.dragAndDropResult, 10);
+        Assert.assertEquals(techGlobalActionsPage.dragAndDropResult.getText(), "An element dropped here!");
+
+    }
+
+
+
+
+
+    /**
+     * Go to https://techglobal-training.netlify.app/
+     * Click on the "Practices" dropdown in the header
+     * Select the "Frontend Testing" option from the dropdown menu
+     * Click on the "Actions" card
+     * Go to input box, and remove existing text inside
+     * Enter “Hello” to search input box
+     * Validate value attribute is “Hello”
+     */
+    @Test(priority = 3, description = "Keyword actions")
+    public void keywordActions() {
+
+        actions.keyDown(Keys.SHIFT).
+                sendKeys(techGlobalActionsPage.inputBox, "h")
+                .keyUp(Keys.SHIFT)
+                .pause(Duration.ofSeconds(2))
+                .sendKeys("ello")
+                .pause(Duration.ofSeconds(2)).perform();
+       Assert.assertEquals(techGlobalActionsPage.inputBox.getAttribute("value"), "Hello");
+
+    }
+
+    /**
+     * Go to https://techglobal-training.netlify.app/
+     * Click on the "Practices" dropdown in the header
+     * Select the "Frontend Testing" option from the dropdown menu
+     * Click on the "Actions" card
+     * Go to input box, and remove existing text inside
+     * Enter “techglobal” to input box with uppercases
+     * Validate the value attribute for search input box is “TECHGLOBAL”
+     * Then, copy the text and paste again
+     * Validate the value attribute for search input box is “TECHGLOBALTECHGLOBAL”
+     */
+
+    @Test(priority = 4, description = "Keyword actions 2")
+    public void keywordActions2() {
+
+        actions.keyDown(Keys.SHIFT).
+                sendKeys(techGlobalActionsPage.inputBox, "techglobal")
+                .keyUp(Keys.SHIFT)
+                .pause(Duration.ofSeconds(2))
+                .keyDown(Keys.COMMAND)
+                .sendKeys("a")
+                .pause(Duration.ofSeconds(2))
+                .sendKeys("c")
+                .keyUp(Keys.COMMAND)
+                .sendKeys(Keys.ARROW_RIGHT)
+                .keyDown(Keys.COMMAND)
+                .sendKeys("v").pause(Duration.ofSeconds(2))
+                .perform();
+
+
+
+        Assert.assertEquals(techGlobalActionsPage.inputBox.getAttribute("value"), "TECHGLOBALTECHGLOBAL");
+
 
 
 
 
     }
+
+
 
 
 
